8216d925ae1a07c78a4fb5ae640fcd015dd0e6c5
Alex <saver2003@gmail.com>
2021-09-05 21:22:12 +0600

    modal module

---
+++ index.js
@@ -0,0 +1,7 @@
+const modal = $.modal({
+    title: 'ttrt',
+    closable: true,
+    content: `<p><h1>Quick start</h1>
+    Looking to quickly add Bootstrap to your project? Use jsDelivr, a free open source CDN. Using a package manager or need to download the source files? Head to the downloads page.</p>`,
+    width: '400px'
+})
