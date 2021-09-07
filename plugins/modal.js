0fa34e110ba58a9bad579e7c7fd0f86ba3a4814e
Alex <saver2003@gmail.com>
2021-09-07 22:53:53 +0600

    initial commit

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,78 +1,104 @@
+Element.prototype.appendAfter = function (element) {
+  element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop() {}
+
+function _createModalFooter(buttons = []) {
+  if (buttons.length === 0) return document.createElement('div');
+
+  const wrap = document.createElement('div');
+  wrap.classList.add('modal-footer');
+
+  buttons.forEach(btn => {
+    const $btn = document.createElement('button');
+    $btn.textContent = btn .text;
+    $btn.classList.add('btn');
+    $btn.classList.add(`btn-${btn.type || 'secondary'}`);
+    $btn.onclick = btn.handler || noop;
+
+    wrap.appendChild($btn);
+  })
+
+  return wrap;
+}
+
 function _createModal(options) {
-    const modal = document.createElement('div');
-    modal.classList.add('vmodal');
-    modal.insertAdjacentHTML('afterbegin', `
-        <div class="modal-overlay" data-close="true">
-            <div class="modal-window" style="width:${options.width}">
-                <div class="modal-header">
-                    <span class="modal-title">${options.title}</span>
-                    ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
-                </div>
-                <div class="modal-body">
-                ${options.content ? options.content : `
-                <p>Lorem ipsum dolor sit.</p>
-                <p>Lorem ipsum dolor sit.</p>
-                `}
-                </div>
-                <div class="modal-footer">
-                    <button>Ok</button>
-                    <button>Cancel</button>
-                </div>
-            </div>
+  const modal = document.createElement('div');
+  modal.classList.add('vmodal');
+  modal.insertAdjacentHTML('afterbegin', `
+    <div class="modal-overlay" data-close="true">
+      <div class="modal-window" style="width:${options.width}">
+        <div class="modal-header">
+            <span class="modal-title">${options.title}</span>
+            ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
         </div>
+        <div class="modal-body" data-content>
+        ${options.content ? options.content : ''}
+        </div>
+      </div>
+    </div>
     `)
-    document.body.appendChild(modal);
-    return modal;
+  const footer = _createModalFooter(options.footerButtons);
+  footer.appendAfter(modal.querySelector('[data-content]'));
+  document.body.appendChild(modal);
+  return modal;
 }
 
 const newModal = {}
 
 $.modal = function (options) {
-    const ANIMATION_SPEED = 200;
-    const $modal = _createModal(options);
-    let closing = false;
-    let destroyed = false;
+  const ANIMATION_SPEED = 200;
+  const $modal = _createModal(options);
+  let closing = false;
+  let destroyed = false;
 
-    const modal = {
-        open() {
-            if (destroyed) {
-                return console.log('Modal is destroyed')
-            }
-            !closing && $modal.classList.add('open')
-        },
-        close() {
-            closing = true;
-            $modal.classList.remove('open')
-            $modal.classList.add('hide')
-            setTimeout(() => {
-                $modal.classList.remove('hide')
-                closing = false;
-            }, ANIMATION_SPEED)
+  const modal = {
+    open() {
+      if (destroyed) {
+        return console.log('Modal is destroyed')
+      }
+      !closing && $modal.classList.add('open')
+    },
+    close() {
+      closing = true;
+      $modal.classList.remove('open')
+      $modal.classList.add('hide')
+      setTimeout(() => {
+        $modal.classList.remove('hide')
+        closing = false;
+        if (typeof options.onClose === 'function') {
+          options.onClose()
         }
+      }, ANIMATION_SPEED)
     }
+  }
 
-    const btn = document.getElementById('modal-open-btn')
+  const btn = document.getElementById('modal-open-btn')
 
-    const openListener = () => {
-        modal.open()
+  const openListener = () => {
+    modal.open()
+  }
+
+  btn.addEventListener('click', openListener)
+
+  const listener = event => {
+    if (event.target.dataset.close) {
+      modal.close();
     }
+  }
 
-    btn.addEventListener('click', openListener)
+  $modal.addEventListener('click', listener)
 
-    const listener = event => {
-        if (event.target.dataset.close) {
-            modal.close();
-        }
+  return Object.assign(modal, {
+    destroy() {
+      $modal.parentNode.removeChild($modal)
+      $modal.removeEventListener('click', listener);
+      $modal.removeEventListener('click', openListener);
+      destroed = true
+    },
+    setContent(html) {
+      $modal.querySelector('[data-content]').innerHTML = html;
     }
-
-    $modal.addEventListener('click', listener)
-
-    return Object.assign(modal, {
-        destroy() {
-            $modal.parentNode.removeChild($modal)
-            $modal.removeEventListener('click', listener);
-            $modal.removeEventListener('click', openListener);
-            destroed = true
-        }
-    })
+  })
 }
